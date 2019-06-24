2939b7b781937d5996db3030996d6e0a9d33f3e2
Paul Bonar <paulbonaretb@gmail.com>
2019-06-24 15:31:30 +0100

    Added Guard Dog

--- Inheritance/src/begin/Controller.java
+++ Inheritance/src/begin/Controller.java
@@ -5,10 +5,11 @@ public class Controller {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Pet myPet = new Pet(1, "Thing", "21/06/2019", false, 200);
+		Pet myPet = new Pet(1, "Thing", "21/06/2019", false, 20.55);
 		
-		Dog myDog = new Dog(2, "Idiot", "21/06/2019", false, 300);
+		Pet myDog = new Dog(2, "Idiot", "21/06/2019", false, 1.55, true);
 		
+	
 		System.out.println(myDog.toString());
 		System.out.println(myPet.toString());
 		System.out.println(myDog.feedingInstructions());
--- Inheritance/src/begin/Dog.java
+++ Inheritance/src/begin/Dog.java
@@ -1,6 +1,18 @@
 package begin;
 
+import javax.print.attribute.standard.Sides;
+
 public class Dog extends Pet {
+
+	private boolean isGuardDog;
+
+	boolean isGuardDog() {
+		return isGuardDog;
+	}
+
+	void setGuardDog(boolean isGuardDog) {
+		this.isGuardDog = isGuardDog;
+	}
 
 	/**
 	 * 
@@ -34,10 +46,28 @@ public class Dog extends Pet {
 	 * @param purchasePrice
 	 */
 	public Dog(int petID, String name, String ownerFName, String ownerLName, String eircode, String dob,
-			boolean licenceReg, double sellingPrice, double purchasePrice) {
+			boolean licenceReg, double sellingPrice, double purchasePrice, boolean isGuardDog) {
 		super(petID, name, ownerFName, ownerLName, eircode, dob, licenceReg, sellingPrice, purchasePrice);
+		// TODO Auto-generated constructor stub
+	
+		this.isGuardDog = isGuardDog;
+	
+	}
+
+	public Dog(int i, String string, String string2, boolean b, double d, boolean c) {
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public String toString() {
+		String myString = super.toString() + "Dog [isGuardDog=" + isGuardDog + "]";
+		return myString;
+	}
 	
+
+	//@Override
+	//public String toString() {
+		//String myString = super.toString();
+		
+		//return String.format("%s,%s,%s,%s", this.getPetID(), this.getName(), this.getOwnerLName(), this.isGuardDog());
 }
